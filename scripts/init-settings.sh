bac91931a70799071b032780d4d80048349d0c6c
Richard <surichard@users.noreply.github.com>
2021-12-24 12:49:19 +0800

    Add files via upload

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -9,8 +9,11 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
-# Disable autostart by default for some packages
-cd /etc/rc.d
-rm -f S98udptools || true
+# Disable IPV6 ula prefix
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+
+# Check file system during boot
+uci set fstab.@global[0].check_fs=1
+uci commit
 
 exit 0
